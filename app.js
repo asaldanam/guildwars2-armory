9c5d4d284880caca81e1143dc6fd649e3c98d3d2
Saldaña Martinez, Abel <e014800@correos.es>
2020-10-04 18:14:34 +0200

    deployment changes

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use('/api/user', userRouter);
 
