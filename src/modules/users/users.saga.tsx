351954292cc61a19efb4ea70c82761f735cf9fb1
Saldaña Martinez, Abel <e014800@correos.es>
2020-10-04 18:53:42 +0200

    env fixes

--- src/modules/users/users.saga.tsx
+++ src/modules/users/users.saga.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_URL = process.env.API_URL;
+const API_URL = process.env.API_URL || '';
 
 /** Guarda un token de usuario */
 export async function saveNewUserToken(token: string) {
-  const response = await axios.post(API_URL + '/user', { token });
+  const response = await axios.post(API_URL + '/api/user', { token });
   return response;
 }
