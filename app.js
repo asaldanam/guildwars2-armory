9fe81a9da6c0f07e33e351d5baecb35607f22859
Saldaña Martinez, Abel <e014800@correos.es>
2020-10-04 19:04:06 +0200

    router

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
